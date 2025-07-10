4be6d39c37c18d64192ee664ae4d636c70823bb1
fskei <keif1201@usherbrooke.ca>
2025-07-10 12:45:26 -0400

    Solution

--- src/monprojet/capteurs/admin.py
+++ src/monprojet/capteurs/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Sensor
+from .models import Sensor, Station, Data, Unit
 
 admin.site.register(Sensor)
+admin.site.register(Station)
+admin.site.register(Data)
+admin.site.register(Unit)
